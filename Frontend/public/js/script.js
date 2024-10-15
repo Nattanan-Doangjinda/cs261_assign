4ff1ca126fcc31aa8333c8d4f442cd6ca80eb434
Nattanan Doangjinda <nattanan.doa@dome.tu.ac.th>
2024-10-15 20:30:46 +0700

    First Commit

---
+++ Frontend/public/js/script.js
@@ -0,0 +1,37 @@
+function submitLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Application-Key': "TU68a4d162f54af68e5d275b5397af78dad6d51724120e8dc8922e598ff4b614fe029ee6a4e14828d521e74b16a254c729",
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "UserName" : username,"PassWord" : password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+
+function call_REST_API_Hello() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = (
+        'http://localhost:8080/hello?' +
+        new URLSearchParams({ myName: username, lastName: password}).toString()
+      );
+    
+    fetch(url)
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('message').innerText = text;
+    })
+    .catch(error => console.error('Error:', error));
+}
